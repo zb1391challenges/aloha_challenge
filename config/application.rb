5c61ce32ed2bc1825567afe015e916abadee851c
zb1391 <zmb1391@gmail.com>
2015-05-08 01:24:27 -0400

    trying this

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module AlohaChallenge
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 	  #
-config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
